9692a6dfa59a128970f47425f9c0107156be491b
Agzul <amayak007@mail.ru>
2019-02-06 16:37:46 +0300

    add edit and new methond in view and some regex fix

--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,10 @@
-from django.urls import path
+from django.urls import path, re_path
 from . import views
 
 
 urlpatterns = [
     path('', views.post_list, name='post_list'),
-    path('post/<id>', views.post_detail, name='post_detail'),
+    re_path(r'^post/(?P<id>\d+)/$', views.post_detail, name='post_detail'),
+    path('post/new', views.post_new, name="post_new"),
+    re_path(r'^post/(?P<id>\d+)/edit/$', views.post_edit, name='post_edit'),
 ]
